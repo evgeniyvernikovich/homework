b3312dac96a5627d304d97e6894f306c1938b8c1
Evgeniy <vernikovich@konstruktiv.by>
2022-11-16 21:24:12 +0300

    'change_settings'

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = '/home/vernikovich/homework/static'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media"
-#MEDIA_ROOT = '/home/vernikovich/homework/media'
+#MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = '/home/vernikovich/homework/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
