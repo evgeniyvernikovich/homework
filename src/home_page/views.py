3fb8d407fbb04e3e9334cf4a45c97849899614d2
Evgeniy <vernikovich@konstruktiv.by>
2022-12-04 20:05:38 +0300

    'add'

--- src/home_page/views.py
+++ src/home_page/views.py
@@ -8,9 +8,9 @@ class HomePage(generic.TemplateView):
     template_name = 'home_page/home_page.html'
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        # context['book1'] = models.Book.objects.get(pk=1)
-        # context['book2'] = models.Book.objects.get(pk=2)
-        # context['book3'] = models.Book.objects.get(pk=3)
-        # context['book4'] = models.Book.objects.get(pk=6)
+        context['book1'] = models.Book.objects.get(pk=1)
+        context['book2'] = models.Book.objects.get(pk=2)
+        context['book3'] = models.Book.objects.get(pk=3)
+        context['book4'] = models.Book.objects.get(pk=6)
         return context
     
